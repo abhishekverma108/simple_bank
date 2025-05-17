ac956cec166c908b90a69f6156f587417aa1775f
abhishekverma108 <abhishekverma.av109@gmail.com>
2025-05-17 22:10:39 +0530

    til-lec-11

--- db/query/account.sql
+++ db/query/account.sql
@@ -11,11 +11,24 @@ INSERT INTO accounts (
 SELECT * FROM accounts
 WHERE id = $1 LIMIT 1;
 
+
+-- name: GetAccountForUpdate :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1
+FOR NO KEY UPDATE;
+
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
+RETURNING *;
+
 -- name: ListAccounts :many
 SELECT * FROM accounts
 ORDER BY id
 LIMIT $1
 OFFSET $2;
+
 
 
 
